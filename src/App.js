4bd873f61be9cf83db7abea9c018104ec4c1f13c
gr8h <hesham.shabana@hotmail.com>
2022-03-20 20:24:34 +0100

    adding react files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React from 'react';
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
   return (
-		<div className="App">
-			<div className="container">
-
-				<div className="header-container">
-					<header>
-            <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
-            </div>
-					</header>
-				</div>
-
-        <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
+    <div>
+      <h1>Hello React on Repl.it!</h1>
+    </div>
+  );
 }
 
-export default App;
+ReactDOM.render(
+  <div className="App">
+    <App/>
+  </div>,
+  document.getElementById('root')
+);
